a0071e7900d5b320eb846094d3ed1be4aaac447a
Xavier Willemin <xavier@slashnet.ch>
2025-05-16 16:36:48 +0200

    Remove unneeded 3DTilesRendererJS typings following latest release

--- src/typings/3d-tiles-renderer.d.ts
+++ src/typings/3d-tiles-renderer.d.ts
@@ -1,8 +1,5 @@
 import { Ellipsoid } from '3d-tiles-renderer';
+import { Camera } from 'three';
 
 import * as NasaTilesRenderer from '3d-tiles-renderer';
-declare module '3d-tiles-renderer' {
-	export interface Ellipsoid {
-		calculateEffectiveRadius(latitude: number): number;
-	}
-}
+declare module '3d-tiles-renderer' {}
